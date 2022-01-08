08b7da96c4f8edf3df9313f6100042c17e65f7b3
Artur Dzhus <arturdz@tahometer.com>
2022-01-08 18:48:41 +0200

    -test 1

---
+++ src/app/state-of-room/state-of-room.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+/**
+ * @title Basic cards
+ */
+@Component({
+  selector: 'app-state-of-room',
+  templateUrl: './state-of-room.component.html',
+  styleUrls: ['./state-of-room.component.scss']
+})
+export class StateOfRoomComponent implements OnInit {
+
+  humidity: any | undefined;
+  temperature: any | undefined;
+  quantityCO: any | undefined;
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<any>('https://ecosystem-smart-home.herokuapp.com/RoomСondition?Id=1').subscribe(data => {
+      this.temperature = data.temperature;
+      this.humidity = data.humidity;
+      this.quantityCO = data.quantityCO;
+    })
+  }
+}
