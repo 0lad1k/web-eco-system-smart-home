7198554e901a716a84265073e1846a5414933b72
Artur Dzhus <arturdz@tahometer.com>
2022-01-08 17:53:39 +0200

    Try add app toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarHederComponent } from './toolbar-heder/toolbar-heder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarHederComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/toolbar-heder/toolbar-heder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-heder',
+  templateUrl: './toolbar-heder.component.html',
+  styleUrls: ['./toolbar-heder.component.scss']
+})
+export class ToolbarHederComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
