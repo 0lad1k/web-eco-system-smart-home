7874609b30844a03f293846aa00c1b6bb7328d0b
Artur Dzhus <arturdz@tahometer.com>
2022-01-08 17:31:56 +0200

    -work

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static("./dist/test-ui"));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/test-ui/'}
+);
+});
+app.listen(process.env.PORT || 8080);
