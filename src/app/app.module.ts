42aae50edd50095c42e3d7c7da03279f8a0e880b
Artur Dzhus <arturdz@tahometer.com>
2022-01-08 18:33:20 +0200

    -add rec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarHederComponent } from './toolbar-heder/toolbar-heder.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RecomendationsPageComponent } from './recomendations-page/recomendations-page.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes: Routes =[
+  { path: 'recomendations', component: RecomendationsPageComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarHederComponent
+    ToolbarHederComponent,
+    RecomendationsPageComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'recomendations', component: RecomendationsPageComponent},
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recomendations-page/recomendations-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recomendations-page',
+  templateUrl: './recomendations-page.component.html',
+  styleUrls: ['./recomendations-page.component.scss']
+})
+export class RecomendationsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
